40c7929368caf5bf6a42693735b73e7497eb6c23
Steve Baker <ste.baker.92@gmail.com>
2018-08-16 18:35:00 +0100

    Added app to shovel data between queues

---
+++ RabbitMqUtil/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RabbitMqUtil
+{
+    class Program
+    {
+        private static string userName = "";
+        private static string password = "";
+        
+        // URL to the rabbitMQ dashboard
+        private static string rabbitUrl = "";
+
+        private static string virtualHost = "";
+        private static string queue = "";
+
+        private static WebClient client;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SetupClient();
+
+            RetryErrorQueueAsync(virtualHost, queue).Wait();
+        }
+
+        private static void SetupClient()
+        {
+            client = new WebClient();
+
+            client.Credentials = new NetworkCredential(userName, password);
+        }
+
+        private static async Task RetryErrorQueueAsync(string virtualHost, string queueName)
+        {
+            var stringErrorLength = ("_error").Length;
+            var liveQueue = queueName.Remove(queueName.Length - stringErrorLength);
+            var amqpurl = $"amqp://{userName}@/{virtualHost}";
+
+            // Use the shovel plugin to move messages between queues
+            var shovelApiUrl = $"{ rabbitUrl }api/parameters/shovel/{virtualHost}/retry{queueName}";
+
+            var value = new ShovelData
+            {
+                SrcUri = amqpurl,
+                SrcQueue = queueName,
+                DestUri = amqpurl,
+                DestQueue = liveQueue,
+                DeleteAfter = "queue-length"
+            };
+
+            var jsonShovel = JsonConvert.SerializeObject(new Shovel { ShovelData = value });
+
+            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+
+            var response = await client.UploadStringTaskAsync(new Uri(shovelApiUrl), "PUT", jsonShovel);
+
+            Console.WriteLine("Successful response: " + response);
+
+        }
+
+    }
+}
---
+++ RabbitMqUtil/Shovel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace RabbitMqUtil
+{
+    public class Shovel
+    {
+        [JsonProperty("value")]
+        public ShovelData ShovelData { get; set; }
+    }
+
+    public class ShovelData
+    {
+        [JsonProperty("src-uri")]
+        public string SrcUri { get; set; }
+
+        [JsonProperty("src-queue")]
+        public string SrcQueue { get; set; }
+
+        [JsonProperty("dest-uri")]
+        public string DestUri { get; set; }
+
+        [JsonProperty("dest-queue")]
+        public string DestQueue { get; set; }
+
+        [JsonProperty("delete-after")]
+        public string DeleteAfter { get; set; }
+    }
+}
